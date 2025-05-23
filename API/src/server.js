1e9d78bc90430f1d95fb6a3c57f43698ec1f146e
0ptim0s <aryanmohanlall@gmail.com>
2025-05-23 20:06:55 +0200

    Accounted for different OS ports

--- API/src/server.js
+++ API/src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const db = require('./config/db');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';
 
 // Test database connection before starting server
