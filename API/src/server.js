444ce20989b06daabd4626f6fb0ff27dcdf611fc
0ptim0s <aryanmohanlall@gmail.com>
2025-06-14 13:36:17 +0200

    made it 1 hour

--- API/src/server.js
+++ API/src/server.js
@@ -40,22 +40,19 @@ io.on('connection',(socket)=>{
     var regions = ['-26.1438,28.0406', 'Sandton', '-25.9819,28.1329', '-25.8347,28.1127', '-25.7566,28.1914', '-26.2678,27.8658', '-26.0936,27.9931', '-26.2259,28.1598', '-26.6667,27.9167', '-26.3333,28.1667', '-25.7487,28.2380'];
     var weatherCurrent = [];
     var johannesburgCount = 0;
-    setInterval(async ()=>{
+    async function getWeather(){
       johannesburgCount = 0;
       console.log(regions.length);
       for(var i=0; i< regions.length; i++){
         const form = new FormData();
         form.append('key', process.env.WEATHERAPI);
         form.append('q',regions[i]);
-        axios.post('https://api.weatherapi.com/v1/current.json',form,{
-          //headers : form.getHeaders()
-        })
-        .then((response)=>{
+        try{
+        var response = await axios.post('https://api.weatherapi.com/v1/current.json',form);
           weatherCurrent.push(response.data);
-        })
-        .catch((error)=>{
+        }catch(error){
           console.log(error);
-        })
+        }
       }
 
       
@@ -74,11 +71,12 @@ io.on('connection',(socket)=>{
      }
 
       weatherCurrent.forEach((w)=>{
-        console.log(w.location.name);
+        console.log(w);
       })
+    }
 
-
-    }, 36000000); //1hr interval
+    getWeather();
+    setInterval(getWeather, 60*60*1000); //1hr interval
 
 
   io.on('disconnect',()=>{
