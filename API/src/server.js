6e0c0fb7a4d4f24e28f47e1cd9838653d5eb22ba
0ptim0s <aryanmohanlall@gmail.com>
2025-06-14 13:25:19 +0200

    weather report interavl

--- API/src/server.js
+++ API/src/server.js
@@ -37,22 +37,48 @@ io.on('connection',(socket)=>{
     socket.emit('incident-recived', `I saved your incident at {${positition.latitude} , ${positition.latitude}}`);
   })
 
-    var regions = ['-26.1438,28.0406', '-26.0912,28.0868', '-25.9819,28.1329', '-25.8347,28.1127', '-25.7566,28.1914', '-26.2678,27.8658', '-26.1625,27.9015', '-26.0936,27.9931', '-26.2259,28.1598', '-26.6667,27.9167', '-26.3333,28.1667', '25.7487,28.2380'];
-    for(var i=0; i< regions.length; i++){
-      const form = new FormData();
-      form.append('key', process.env.WEATHERAPI);
-      form.append('q',regions[i]);
-      axios.post('https://api.weatherapi.com/v1/current.json',form,{
-        //headers : form.getHeaders()
-      })
-      .then((response)=>{
-        console.log(response.data);
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
-    }
+    var regions = ['-26.1438,28.0406', 'Sandton', '-25.9819,28.1329', '-25.8347,28.1127', '-25.7566,28.1914', '-26.2678,27.8658', '-26.0936,27.9931', '-26.2259,28.1598', '-26.6667,27.9167', '-26.3333,28.1667', '-25.7487,28.2380'];
+    var weatherCurrent = [];
+    var johannesburgCount = 0;
+    setInterval(async ()=>{
+      johannesburgCount = 0;
+      console.log(regions.length);
+      for(var i=0; i< regions.length; i++){
+        const form = new FormData();
+        form.append('key', process.env.WEATHERAPI);
+        form.append('q',regions[i]);
+        axios.post('https://api.weatherapi.com/v1/current.json',form,{
+          //headers : form.getHeaders()
+        })
+        .then((response)=>{
+          weatherCurrent.push(response.data);
+        })
+        .catch((error)=>{
+          console.log(error);
+        })
+      }
 
+      
+     for(var i=0; i< weatherCurrent.length; i++){
+      if(weatherCurrent[i].location.name == 'Johannesburg'){
+        johannesburgCount++;
+      }
+
+      if(johannesburgCount == 2 && weatherCurrent[i].location.name == 'Johannesburg'){
+        weatherCurrent[i].location.name = 'Rosebank';
+      }
+
+      if(johannesburgCount == 3 && weatherCurrent[i].location.name == 'Johannesburg'){
+        weatherCurrent[i].location.name = 'Marlboro';
+      }
+     }
+
+      weatherCurrent.forEach((w)=>{
+        console.log(w.location.name);
+      })
+
+
+    }, 36000000); //1hr interval
 
 
   io.on('disconnect',()=>{
