619c13c1770f2f5358e7fb5b1b6b2a09053c67ab
0ptim0s <aryanmohanlall@gmail.com>
2025-06-25 14:53:25 +0200

    Added get archives endpoint

--- API/src/app.js
+++ API/src/app.js
@@ -10,6 +10,7 @@ const userRoutes = require('./routes/user');
 const incidentRoutes = require('./routes/incidents');
 const alertRoutes = require('./routes/alerts');
 const trafficRoutes = require('./routes/traffic'); // NEW LINE ADDED
+const archivesRoutes = require('./routes/archives');
 
 // Create Express application
 const app = express();
@@ -31,6 +32,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/incidents', incidentRoutes);
 app.use('/api/alerts', alertRoutes);
 app.use('/api/traffic', trafficRoutes); // NEW LINE ADDED
+app.use('/api/archives', archivesRoutes);
 
 // Health check endpoint
 app.get('/api/health', (req, res) => {
