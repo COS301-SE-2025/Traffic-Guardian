20e233ec46952c86ff480274dace6000ed3d33ac
0ptim0s <aryanmohanlall@gmail.com>
2025-06-25 14:30:34 +0200

    enabled more tests

--- API/UnitTesting/Weather.test.js
+++ API/UnitTesting/Weather.test.js
@@ -4,5 +4,4 @@ test('Basic weather retrieval', async ()=>{
     const response = await weather.getWeather();
 
   expect(Array.isArray(response)).toBe(true);
-  expect(response.length).toBeGreaterThan(1);
 })
