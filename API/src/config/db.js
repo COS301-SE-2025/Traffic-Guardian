2afa5fc156c1a8a509f39b15646bb16ea28a91f4
NicholasDobson <u23671964@tuks.co.za>
2025-06-26 14:43:33 +0200

    data

--- API/src/config/db.js
+++ API/src/config/db.js
@@ -39,13 +39,7 @@ const pool = new Pool({
   ssl: {rejectUnauthorized: false}
 });
 
-// Log connection attempt but hide sensitive information
-console.log('Database configuration loaded:');
-console.log(`- Host: ${process.env.DATABASE_HOST}`);
-console.log(`- Database: ${process.env.DATABASE_NAME}`);
-console.log(`- User: ${process.env.DATABASE_USERNAME}`);
-console.log(`- Password: [REDACTED]`);
-console.log(`- Port: ${process.env.DATABASE_PORT}`);
+
 
 // Test the database connection on startup
 pool.query('SELECT NOW()')
