15b94974c5db4d93072f64fae3807923fb6f0617
0ptim0s <aryanmohanlall@gmail.com>
2025-06-26 22:02:01 +0200

    Added scalability testing

---
+++ API/IntegrationTesting/Scalability.test.js
@@ -0,0 +1,30 @@
+const autocannon = require('autocannon');
+
+describe('Performance Test with Autocannon', () => {
+  test('Case with 50 connections', async()=>{
+    const params = {
+      url: 'http://localhost:5000',
+      connections: 50,
+      duration: 3,
+    };
+
+    const result = await runAutoCannon(params);
+
+    console.log('Requests/sec:', result.requests.average, 'at ', new Date());
+
+    expect(result.requests.average).toBeGreaterThanOrEqual(0);
+  });
+});
+
+
+function runAutoCannon(p){
+    return new Promise((resolve, reject)=>{
+        autocannon(p, (err, res)=>{
+            if(err){
+                return reject(err);
+            }
+
+            return resolve(res);
+        })
+    });
+}
