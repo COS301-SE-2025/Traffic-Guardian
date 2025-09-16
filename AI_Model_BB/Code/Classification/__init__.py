0201da6250c3034e1817df95a4302361be47d8cb
NicholasDobson <u23671964@tuks.co.za>
2025-09-16 12:11:12 +0200

    Add initial documentation for the classification package in Traffic Guardian

---
+++ AI_Model_BB/Code/Classification/__init__.py
@@ -0,0 +1,4 @@
+"""
+Classification package for Traffic Guardian's enhanced car crash classification system.
+This package contains modularized components for crash detection and classification.
+"""
